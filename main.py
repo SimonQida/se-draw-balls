5de595066ad019a8132d1f6f5f0eda77ec59664b
lizekang <zekangli@hustunique.com>
2017-06-12 15:37:12 +0800

    add plot for 2d

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from common import Point_2D, Point_3D, Circle, Ball, Coordinate, combination_traversal
 from calculate_2d import calculate_2d
 from calculate_3d import calculate_3d
+import turtle
 
 def main():
     """
@@ -19,7 +20,7 @@ def main():
         Coordinate('z', border=0, is_max=False),
         Coordinate('z', border=200, is_max=True),
     ]
-    balls, restrictions = calculate_3d(12, balls, restrictions_3d)
+    balls, restrictions = calculate_3d(10, balls, restrictions_3d)
     for ball in balls:
         # print(ball.center.distant_to_pow({ 'x': 40, 'y': 100, 'z': 40 }), ball.radius**2)
         print(ball.dictify())
@@ -34,10 +35,30 @@ def main():
     ]
     circles = []
     circles, restrictions = calculate_2d(12, circles, restrictions_2d)
-    for circle in circles:
+    plot(circles)
+    # for circle in circles:
         # print(ball.center.distant_to_pow({ 'x': 40, 'y': 100, 'z': 40 }), ball.radius**2)
-        print(circle.dictify())
+        # print(circle.dictify())
 
+def plot(result):
+    turtle.penup()
+    turtle.goto(0, 300)
+    turtle.pendown()
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.penup()
+    for item in result:
+        turtle.penup()
+        turtle.goto(item.center.x*1.5, (item.center.y-item.radius)*1.5)
+        turtle.pendown()
+        turtle.circle(item.radius*1.5)
+    turtle.exitonclick()
 if __name__ == "__main__":
     main()
 
